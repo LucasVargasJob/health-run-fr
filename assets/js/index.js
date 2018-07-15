455607ce8da8ceee64a9fbfd785ed5c54f26fb51
Lucas Vargas <lucasvargasjob@gmail.com>
2018-07-15 16:44:47 -0300

    Primeiro commit
    add

---
+++ assets/js/index.js
@@ -0,0 +1,73 @@
+function carregaRotasSelect(){
+	
+	select = document.getElementById('listaRotas');
+	
+	for (var i = 0 ; i<=rotas.length ; i++) {
+		console.log(rotas[i]);
+		var opt = document.createElement('option');
+		opt.value = rotas[i]['cod_rota'];
+		opt.innerHTML = rotas[i]['nome'];
+		select.appendChild(opt);
+	}
+}
+
+var modal = document.getElementById('myModal');
+var btn = document.getElementById("myBtn");
+var botaoCadastrar = document.getElementById("cadastrarDesenhoRota");
+var span = document.getElementsByClassName("close")[0];
+
+botaoCadastrar.onclick = function() {
+    var nome = document.getElementById("nomeRota").value;
+	
+	    var novaRota = {
+            "cod_rota": 4,
+            "nome": nome,
+            "pontos": points  
+        };
+        console.log(points);
+        select = document.getElementById('listaRotas');
+        var opt = document.createElement('option');
+        opt.value = novaRota['cod_rota'];
+        opt.innerHTML = novaRota['nome'];
+        select.appendChild(opt);
+		modal.style.display = "none";
+		alert("Rota compartilhada com sucesso.");
+        rotas.push(novaRota);
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+            map: resultsMap,
+            position: results[0].geometry.location
+        });
+    } else {
+        alert('Problema na pesquisa de endereço: ' + status);
+    }
+    });
+}
+
+
+rotas.forEach(function(rotas) {
+    var marker = new google.maps.Marker({
+       position: new google.maps.LatLng(rotas['pontos'][0][0], rotas['pontos'][0][1]),
+       icon: 'https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png',
+       map: map
+    });
+});
+
+//initMap();
+//carregaRotasSelect();
